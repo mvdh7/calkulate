4205b2e4931ca8f2fd53bfa9c9163b56622e0ef2
Matthew Humphreys <mvdh7@users.noreply.github.com>
2019-01-14 16:07:49 +0000

    Add AT prediction and components

--- calk_ptl.m
+++ calk_ptl.m
@@ -1,7 +1,9 @@
 %% Import data
 
 % Initialise Python
-calk_initpy('//anaconda/envs/spritzer/bin/python')
+% calk_initpy('//anaconda/envs/spritzer/bin/python') % Mac
+calk_initpy( ... Windows
+    'C:\Users\yau17reu\anaconda\Anaconda3\envs\spritzer\pythonw.exe')
 % calk = py.importlib.import_module('calkulate');
 
 % Settings
@@ -22,38 +24,49 @@ clear t
     = calk_guessGran(datfile,Vsamp,Cacid,S);
 t = struct2table(t);
 
+t.mu = Msamp ./ (Msamp + t.Macid);
+
 % Solve for TA fully
 [AT,EMF0,AT_RMS,AT_Npts] = calk_VINDTA(datfile,Vsamp,Cacid, ...
     S,CT,PT,SiT,burette_cx,Tk_force);
 
 [t.H,t.pH] = calk_EMF2H(t.EMF,EMF0,t.Tk);
 
-% Calculate simulated TA
-test = cell(py.calkulate.VINDTA.simAT(py.numpy.array(t.Macid'), ...
-    py.numpy.array(t.Tk'),py.numpy.array(t.H'),Msamp,S,CT,PT,SiT));
-t.AT = double(py.array.array('d',test{1}))';
+[t.simAT,components] = calk_simAT(t.Macid,t.Tk,t.H,Msamp,S,CT,PT,SiT);
+cvars = fieldnames(components);
+t = [t struct2table(components)];
+
+t.estAT = (t.simAT + t.Macid*Cacid ./ (t.Macid + Msamp)) ./ t.mu;
 
 % ----------------------------------------------------- Plot the lot! -----
 
-fvars = {'EMF' 'F1g' 'EMF0g' 'AT'};
-flabels = {'EMF / mV' 'F1' 'EMF0 guess / mV' 'AT'};
+fvars = {'EMF' 'estAT' 'EMF0g' [] 'F1g'};
+flabels = {'EMF / mV' [] 'EMF0 guess / mV' 'estAT' 'F1'};
 
-fxlim = minmax(t.Macid');
+fxlim = minmax(1e3*t.Macid');
 
 figure(1); clf
 
 for V = 1:numel(fvars)
+if ~isempty(fvars{V})
+    
+subplot(3,2,V); hold on
 
-subplot(2,2,V); hold on
+    xlim(fxlim)
 
-    scatter(t.Macid(t.Lg),t.(fvars{V})(t.Lg),'c','filled')
-    scatter(t.Macid,t.(fvars{V}),'k')
+    scatter(1e3*t.Macid(t.Lg),t.(fvars{V})(t.Lg),'c','filled')
+    scatter(1e3*t.Macid,t.(fvars{V}),'k')
     
     if ismember(fvars{V},'EMF')
-        scatter(t.Macid(t.L),t.(fvars{V})(t.L),'r')
+        scatter(1e3*t.Macid(t.L),t.(fvars{V})(t.L),'r')
     end %if
     
     switch fvars{V}
+        
+        case 'EMF'
+            
+            ylim(minmax(t.EMF')+30*[-1 1])
+            set(gca, 'ytick',0:100:800)
         
         case 'F1g'
             % plot regression line
@@ -62,12 +75,32 @@ subplot(2,2,V); hold on
         case 'EMF0g' 
             plot(fxlim,EMF0g*[1 1],'b')
             
+        case 'estAT'
+            plot(fxlim,AT*[1 1])
+            
     end %switch
-
-    xlim(fxlim)
 
     xlabel('Acid mass / g')
     ylabel(flabels{V})
     
+    set(gca, 'box','on', 'tickdir','out', 'xcolor','k', 'ycolor','k', ...
+        'fontname','arial', 'fontsize',8, 'xtick',0:10)
+
+end %if
 end %for V
-    
+
+subplot(3,2,[4 6]); hold on
+        
+    plot(1e3*t.Macid,t.estAT)
+
+    for V = 1:numel(cvars)
+
+        plot(1e3*t.Macid,abs(t.(cvars{V})))
+
+    end %for V
+
+    legend(['estAT'; cvars], 'location','eastoutside')
+
+    set(gca, 'yscale','log')
+
+    xlim(fxlim)
