e7a0afe7e82df6f2668af6944e9bcf2aad49b688
Matthew Humphreys <mvdh7@users.noreply.github.com>
2019-01-14 16:08:05 +0000

    Make array inputs more robust

--- matlab/calk_simH.m
+++ matlab/calk_simH.m
@@ -1,7 +1,7 @@
 function [H,pH] = calk_simH(Macid,Tk,Msamp,Cacid,S,AT,CT,PT,SiT)
 
-H = py.calkulate.VINDTA.simH(py.numpy.array(Macid'), ...
-    py.numpy.array(Tk'),Msamp,Cacid,S,AT,CT,PT,SiT);
+H = py.calkulate.VINDTA.simH(py.numpy.array(Macid(:)'), ...
+    py.numpy.array(Tk(:)'),Msamp,Cacid,S,AT,CT,PT,SiT);
 
 H = double(py.array.array('d',H))';
 
