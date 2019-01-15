240bf58d27dac277e31e994259eb30a9112f802c
Matthew Humphreys <mvdh7@users.noreply.github.com>
2019-01-15 09:40:23 +0000

    Improve layout and add a few comments

--- run_ptl.m
+++ run_ptl.m
@@ -3,11 +3,10 @@
 calk_initpy( ... Windows
     'C:\Users\yau17reu\anaconda\Anaconda3\envs\spritzer\pythonw.exe')
 
-% Settings
+% Input settings
 datpath = 'datfiles/';
 datfile = '0-0  0  (0)CRM-144-0435-4.dat';
-Vsamp = 100;
-% Cacid = 0.1;
+Vsamp = 100.0123; % ml
 [CT,AT_cert,S,PT,SiT] = dicksonCRM(144);
 CT      = CT      * 1e-6;
 AT_cert = AT_cert * 1e-6;
@@ -17,7 +16,10 @@ burette_cx = 1;
 Tk_force = [];
 printpath = 'figures/';
 
-% Get proper Cacid
+% % Either guess Cacid...
+% Cacid = 0.1;
+
+% ... or get proper Cacid from a CRM:
 Cacid = calk_VINDTA_CRM([datpath datfile],Vsamp,AT_cert,S,CT,PT,SiT, ...
     burette_cx,Tk_force);
 
