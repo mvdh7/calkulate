845e127ec638b307c66c27ddec23fef9c7fd7467
Matthew Humphreys <mvdh7@users.noreply.github.com>
2019-04-09 16:13:39 +0100

    Hide MATLAB stuff

--- matlab/calk_dens_acid.m
+++
@@ -1,7 +0,0 @@
-function rho_acid = calk_dens_acid(Tk)
-
-rho_acid = py.calkulate.dens.acid(py.numpy.array(Tk(:)'));
-
-rho_acid = double(py.array.array('d',rho_acid))';
-
-end %function calk_dens_acid
