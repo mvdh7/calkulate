d1542dd16551595ef31d980c1a041481facdde14
Matthew <m.humphs@gmail.com>
2019-08-05 06:36:19 +0100

    Update email and install_requires

--- setup.py
+++ setup.py
@@ -5,16 +5,18 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name         = 'Calkulate',
-    version      = __version__,
-    author       = 'Matthew P. Humphreys',
-    author_email = 'm.p.humphreys@cantab.net',
-    description  = 'Seawater total alkalinity from titration data',
-    url          = 'https://github.com/mvdh7/calkulate',
-    packages     = setuptools.find_packages(),
+    name = 'Calkulate',
+    version = __version__,
+    author = 'Matthew P. Humphreys',
+    author_email = 'm.p.humphreys@icloud.com',
+    description = 'Seawater total alkalinity from titration data',
+    url = 'https://github.com/mvdh7/calkulate',
+    packages = setuptools.find_packages(),
     install_requires = [
+        'matplotlib>=3',
         'numpy>=1.15',
-        'scipy>=1.1'],
+        'scipy>=1.1',
+    ],
     long_description = long_description,
     long_description_content_type = 'text/markdown',
     classifiers = (
@@ -25,4 +27,6 @@ setuptools.setup(
         'Operating System :: OS Independent',
         'Natural Language :: English',
         'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: Chemistry'),)
+        'Topic :: Scientific/Engineering :: Chemistry',
+    ),
+)
