1d79589dd02c058cbbcdf5e684e9c3fcaf9dcee8
Matthew Humphreys <m.p.humphreys@icloud.com>
2020-09-04 10:03:22 +0200

    Use np.isclose

--- tests/test_conversions.py
+++ tests/test_conversions.py
@@ -18,7 +18,7 @@ def test_emf_h_conversion():
     assert np.all(h > 0)
     emf_converted = calk.convert.h_to_emf(h, emf0, temperature)
     emf_diff = emf_converted - emf_original
-    assert np.max(np.abs(emf_diff)) < 1e-12
+    assert np.all(np.isclose(emf_diff, 0, rtol=0, atol=1e-12))
 
 
 test_emf_h_conversion()
