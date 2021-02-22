fcb0eaae44aea1eb27b47e85eeeae9c9f6bf3954
Matthew Humphreys <m.p.humphreys@icloud.com>
2021-02-22 12:38:25 +0100

    Clear the way for v23

--- calkulate/titrations/__init__.py
+++
@@ -1,3 +0,0 @@
-from .titration import Titration
-from .read import read_dat, kwargs_TiTouch
-from .write import to_dat
--- calkulate/titrations/titration.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-
-class Titration(pd.DataFrame):
-    from .write import to_dat
