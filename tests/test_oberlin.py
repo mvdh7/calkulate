0d6e1a122f73cfd5a94ce61bf936feb1d1c9c159
Matthew P. Humphreys <m.p.humphreys@icloud.com>
2023-07-03 17:53:08 +0200

    Add test function

--- tests/test_oberlin.py
+++ tests/test_oberlin.py
@@ -5,3 +5,11 @@ ds = calk.read_csv("tests/data/oberlin/metadf.csv")
 
 # Solve for alkalinity
 ds.solve()
+
+
+def test_oberlin():
+    assert "alkalinity" in ds
+    assert not ds.alkalinity.isnull().any()
+
+
+# test_oberlin()
