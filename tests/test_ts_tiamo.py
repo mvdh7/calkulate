c26d038636181d2cc3713f89dfb0ab5cd84cb258
Matthew Humphreys <mvdh7@users.noreply.github.com>
2025-05-23 09:29:32 +0200

    Don't need numpy here

--- tests/test_ts_tiamo.py
+++ tests/test_ts_tiamo.py
@@ -1,8 +1,6 @@
 # %%
 from os import listdir
 
-import numpy as np
-
 import calkulate as calk
 
 
@@ -12,9 +10,9 @@ def test_read_dat():
     for filename in filenames:
         td = calk.read.read_tiamo_de(filepath + filename)
         assert isinstance(td, calk.read.TiamoData)
-        assert np.all(td[0] == td.volume)
-        assert np.all(td[1] == td.pH)
-        assert np.all(td[2] == td.temperature)
+        assert all(td[0] == td.volume)
+        assert all(td[1] == td.pH)
+        assert all(td[2] == td.temperature)
         assert td.volume[0] > 0
 
 
